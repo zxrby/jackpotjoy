efb1a7256041f231e55fd5dd990d0f3e6a0649d0
zxrby <79419506+zxrby@users.noreply.github.com>
2022-04-30 15:10:56 +0200

    Create k.lua

---
+++ k.lua
@@ -0,0 +1,60 @@
+
+local string_1 = "Black Pride Double Buns";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Rich Scientist Goggles";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Sir Rich McMeowington";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "I Am Rich Cap";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Omega Rainbow Equinox";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Rainbow Man Hair";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Sir Rich McMoneyston";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Gold ROBLOX Music Video Award";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+local string_1 = "Rainbow Short Shaggy Hair";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
+
+
+
+local string_1 = "Rainbow Melancholy Hair";
+local string_2 = "1000000000";
+local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+Target:FireServer(string_1, string_2);
