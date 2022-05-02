de9e15b166529ad7f6da27631f3f00869e4468d4
zxrby <79419506+zxrby@users.noreply.github.com>
2022-05-02 21:28:52 +0200

    Update k.lua

--- k.lua
+++ k.lua
@@ -1,243 +1,237 @@
- 
-    local amount = 40
-    spawn(function()
-       while wait() do
-           pcall(function()
-                 game.Players.LocalPlayer.Deb.Value = false
-           game.Players.LocalPlayer.Deb2.Value = false
-                local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-            spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-            end)
-             spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-             end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-            end)
-            end)
-        end
-    end)
-    spawn(function()
-       while wait() do
-           pcall(function()
-                 game.Players.LocalPlayer.Deb.Value = false
-           game.Players.LocalPlayer.Deb2.Value = false
-                local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-              local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-            spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-            end)
-             spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-             end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-         end)
-         spawn(function()
-                 local args = {
-                [1] = "Bitosolized",
-                [2] = amount
-            }
-            
-            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
-             
-            end)
-            end)
-        end
-    end)
-    
+local amount = 40
+spawn(function()
+   while wait() do
+       pcall(function()
+             game.Players.LocalPlayer.Deb.Value = false
+       game.Players.LocalPlayer.Deb2.Value = false
+            local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
         
-    end)
-
-
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+        spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+        end)
+         spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+         end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+        end)
+        end)
+    end
+end)
+spawn(function()
+   while wait() do
+       pcall(function()
+             game.Players.LocalPlayer.Deb.Value = false
+       game.Players.LocalPlayer.Deb2.Value = false
+            local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+          local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+        spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+        end)
+         spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+         end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+     end)
+     spawn(function()
+             local args = {
+            [1] = "Bitosolized",
+            [2] = amount
+        }
+        
+        game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+         
+        end)
+        end)
+    end
+end)
