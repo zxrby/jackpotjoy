6af761cd0f24ee0583456652d56dbd90f4d74f35
zxrby <79419506+zxrby@users.noreply.github.com>
2022-05-02 21:20:41 +0200

    Update k.lua

--- k.lua
+++ k.lua
@@ -1,109 +1,273 @@
+local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
 
-local string_1 = "Black Pride Double Buns";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+
+local tab = win:Tab("main")
+
+tab:Button("20T ADD", function()
+    local string_1 = "8-Bit King Nate";
+    local string_2 = "100000";
+    local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
 Target:FireServer(string_1, string_2);
 
-
-local string_1 = "Rich Scientist Goggles";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
+end)
 
 
-local string_1 = "Sir Rich McMeowington";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
+tab:Button("100T ADD", function()    
+    local string_1 = "8-Bit King Nate";
+    local string_2 = "450000";
+    local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
+    Target:FireServer(string_1, string_2);
+    
+    end)
+    
+tab:Button("100T ADD", function()    
+    local amount = 40
+    spawn(function()
+       while wait() do
+           pcall(function()
+                 game.Players.LocalPlayer.Deb.Value = false
+           game.Players.LocalPlayer.Deb2.Value = false
+                local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+            spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+            end)
+             spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+             end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+            end)
+            end)
+        end
+    end)
+    spawn(function()
+       while wait() do
+           pcall(function()
+                 game.Players.LocalPlayer.Deb.Value = false
+           game.Players.LocalPlayer.Deb2.Value = false
+                local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+              local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+            spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+            end)
+             spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+             end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+         end)
+         spawn(function()
+                 local args = {
+                [1] = "Bitosolized",
+                [2] = amount
+            }
+            
+            game:GetService("ReplicatedStorage").Remotes.OpenCase:InvokeServer(unpack(args))
+             
+            end)
+            end)
+        end
+    end)
+    
+        
+    end)
 
 
-local string_1 = "I Am Rich Cap";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
+tab:Label("EVERYTHING MADE BY ZXRBY ")
 
+local changeclr = win:Tab("Change UI Color")
 
-local string_1 = "Omega Rainbow Equinox";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Rainbow Man Hair";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Sir Rich McMoneyston";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Gold ROBLOX Music Video Award";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Rainbow Short Shaggy Hair";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-
-local string_1 = "Rainbow Melancholy Hair";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "3D Glitch TV Head";
-local string_2 = "4444444444444";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Glitch Head";
-local string_2 = "4444444444444";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Purple Glitch Antlers";
-local string_2 = "4444444444444";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Glitched Monster Mask";
-local string_2 = "4444444444444";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Forward Tilt Samurai Hat";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Samurai Hat";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Angel Samurai Hat";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-
-local string_1 = "Red Samurai Helmet";
-local string_2 = "1000000000";
-local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
+changeclr:Colorpicker("Change UI Color",Color3.fromRGB(44, 120, 224), function(t)
+lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
+end)
