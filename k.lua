9cf8f2d06c01a229e64b9e147a2fab43b99eaf91
zxrby <79419506+zxrby@users.noreply.github.com>
2022-05-02 21:25:22 +0200

    Update k.lua

--- k.lua
+++ k.lua
@@ -1,27 +1,4 @@
-local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/Vape.txt")()
-
-local win = lib:Window("PREVIEW",Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
-
-local tab = win:Tab("main")
-
-tab:Button("20T ADD", function()
-    local string_1 = "8-Bit King Nate";
-    local string_2 = "100000";
-    local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-Target:FireServer(string_1, string_2);
-
-end)
-
-
-tab:Button("100T ADD", function()    
-    local string_1 = "8-Bit King Nate";
-    local string_2 = "450000";
-    local Target = game:GetService("ReplicatedStorage").Remotes.Jackpot;
-    Target:FireServer(string_1, string_2);
-    
-    end)
-    
-tab:Button("100T ADD", function()    
+ 
     local amount = 40
     spawn(function()
        while wait() do
@@ -264,10 +241,3 @@ tab:Button("100T ADD", function()
     end)
 
 
-tab:Label("EVERYTHING MADE BY ZXRBY ")
-
-local changeclr = win:Tab("Change UI Color")
-
-changeclr:Colorpicker("Change UI Color",Color3.fromRGB(44, 120, 224), function(t)
-lib:ChangePresetColor(Color3.fromRGB(t.R * 255, t.G * 255, t.B * 255))
-end)
